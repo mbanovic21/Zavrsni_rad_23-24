a022a96360fac79c0c1e3479b54a2a08066e87ad
= <mbanovic21@foi.hr>
2024-05-04 10:34:15 +0200

    namespaces refactoring

--- Software/DataAccessLayer/PMSmodel.cs
+++ Software/DataAccessLayer/PMSmodel.cs
@@ -1,3 +1,4 @@
+using EntityLayer.Entities;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
--- Software/EntityLayer/Entities/User.cs
+++ Software/EntityLayer/Entities/User.cs
@@ -1,4 +1,4 @@
-namespace DataAccessLayer
+namespace EntityLayer.Entities
 {
     using System;
     using System.Collections.Generic;
