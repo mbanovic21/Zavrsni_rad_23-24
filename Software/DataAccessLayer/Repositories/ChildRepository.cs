1c2b6b670b80007412317f61d7f726e30cd6556c
= <mbanovic21@foi.hr>
2024-05-21 20:40:07 +0200

    last commit today

--- Software/DataAccessLayer/Repositories/ChildRepository.cs
+++ Software/DataAccessLayer/Repositories/ChildRepository.cs
@@ -120,31 +120,6 @@ namespace DataAccessLayer.Repositories
             return isSaveSuccessful;
         }
 
-        private bool SaveChangesWithValidation(DbContext context, ref int affectedRows)
-        {
-            try
-            {
-                affectedRows = context.SaveChanges();
-            } catch (DbEntityValidationException ex)
-            {
-                // Iterirajte kroz sve entitete koji su imali valjanosne greške
-                foreach (var validationErrors in ex.EntityValidationErrors)
-                {
-                    // Iterirajte kroz sve greške valjanosti za svaki entitet
-                    foreach (var validationError in validationErrors.ValidationErrors)
-                    {
-                        Console.WriteLine($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
-                    }
-                }
-
-                // Vratite false jer je došlo do greške pri spremanju
-                return false;
-            }
-
-            // Vratite true ako je barem jedan red promijenjen u bazi podataka
-            return affectedRows > 0;
-        }
-
         //update child
         public bool updateChild(Child child)
         {
@@ -170,6 +145,31 @@ namespace DataAccessLayer.Repositories
             {
                 return false;
             }
+        }
+
+        private bool SaveChangesWithValidation(DbContext context, ref int affectedRows)
+        {
+            try
+            {
+                affectedRows = context.SaveChanges();
+            } catch (DbEntityValidationException ex)
+            {
+                // Iterirajte kroz sve entitete koji su imali valjanosne greške
+                foreach (var validationErrors in ex.EntityValidationErrors)
+                {
+                    // Iterirajte kroz sve greške valjanosti za svaki entitet
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Console.WriteLine($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
+                    }
+                }
+
+                // Vratite false jer je došlo do greške pri spremanju
+                return false;
+            }
+
+            // Vratite true ako je barem jedan red promijenjen u bazi podataka
+            return affectedRows > 0;
         }
 
         public void Dispose()
