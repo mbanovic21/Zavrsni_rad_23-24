6f674c3840821fe9736e7a5f03a40b6e9abbe641
= <mbanovic21@foi.hr>
2024-06-23 21:50:38 +0200

    Everything done! Just editing and bug fixing to do!

---
+++ Software/PreschoolManagmentSoftware/Converters/ValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PreschoolManagmentSoftware.Converters
+{
+    public class DoubleToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return ((int)doubleValue).ToString();
+            }
+            return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Software/PreschoolManagmentSoftware/UserControls/Dashboard/ucChildrenAttendanceNumber.xaml.cs
+++ Software/PreschoolManagmentSoftware/UserControls/Dashboard/ucChildrenAttendanceNumber.xaml.cs
@@ -1,30 +1,32 @@
 ﻿using BusinessLogicLayer.DBServices;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace PreschoolManagmentSoftware.UserControls.Dashboard
 {
-    /// <summary>
-    /// Interaction logic for ucChildrenAttendanceNumber.xaml
-    /// </summary>
-    public partial class ucChildrenAttendanceNumber : UserControl
+    public partial class ucChildrenAttendanceNumber : UserControl, INotifyPropertyChanged
     {
-        private AttendanceServices _attendanceServices = new AttendanceServices();
+        private readonly AttendanceServices _attendanceServices = new AttendanceServices();
+        private double _animatedNumber;
+
+        public double AnimatedNumber
+        {
+            get { return _animatedNumber; }
+            set
+            {
+                _animatedNumber = value;
+                OnPropertyChanged(nameof(AnimatedNumber));
+            }
+        }
+
         public ucChildrenAttendanceNumber()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void ucChildrenNumber_Loaded(object sender, RoutedEventArgs e)
@@ -35,7 +37,30 @@ namespace PreschoolManagmentSoftware.UserControls.Dashboard
         private async void LoadNumber()
         {
             var currentDate = DateTime.Now.ToString("dd.M.yyyy.");
-            txtNumber.Text = await Task.Run(() => _attendanceServices.GetAttendancesCountByDate(currentDate).ToString());
+            int attendanceCount = await Task.Run(() => _attendanceServices.GetAttendancesCountByDate(currentDate));
+            AnimateNumber(attendanceCount);
         }
+
+        private void AnimateNumber(int targetNumber)
+        {
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0,
+                To = targetNumber,
+                Duration = new Duration(TimeSpan.FromSeconds(2)),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+            this.BeginAnimation(AnimatedNumberProperty, animation);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static readonly DependencyProperty AnimatedNumberProperty =
+            DependencyProperty.Register("AnimatedNumber", typeof(double), typeof(ucChildrenAttendanceNumber), new PropertyMetadata(0.0));
     }
 }
